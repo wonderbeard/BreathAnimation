816267d4a73e8040b9829c028dd799c74070b2c1
Andrew Malyarchuk <andrew.malyarchuk@gmail.com>
2018-04-19 16:03:32 +0300

    Initial commit

---
+++ BreathAnimation/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BreathAnimation
+//
+//  Created by Andrew Malyarchuk on 19.04.2018.
+//  Copyright © 2018 wonderbeard. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+
+    var window: UIWindow?
+    
+}
+
+extension AppDelegate: UIApplicationDelegate {
+}
+
