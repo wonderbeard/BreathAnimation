c0295470692127f1fbd786d544c86ca4d7af63f4
Andrew Malyarchuk <andrew.malyarchuk@gmail.com>
2018-04-19 20:53:57 +0300

    add Countdown

---
+++ BreathAnimation/Countdown.swift
@@ -0,0 +1,37 @@
+//
+//  Countdown.swift
+//  BreathAnimation
+//
+//  Created by Andrew Malyarchuk on 19.04.2018.
+//  Copyright © 2018 wonderbeard. All rights reserved.
+//
+
+import Foundation
+
+class Countdown {
+    
+    private let duration: TimeInterval
+    private let interval: TimeInterval
+    
+    init(duration: TimeInterval, interval: TimeInterval) {
+        self.duration = duration
+        self.interval = interval
+    }
+    
+    @discardableResult
+    func start(_ handler: @escaping (_ remainingTime: TimeInterval) -> Void) -> Cancellable {
+        var remainingTime = duration
+        handler(remainingTime)
+        let timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { timer in
+            remainingTime -= timer.timeInterval
+            handler(remainingTime)
+            if remainingTime <= 0 {
+                timer.invalidate()
+            }
+        }
+        return ClosureCancellable {
+            timer.invalidate()
+        }
+    }
+    
+}
